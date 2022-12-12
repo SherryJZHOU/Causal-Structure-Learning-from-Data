447808844e7fb7d8b2c5637b08722ee5fa76c219
SherryJZHOU <120346791+SherryJZHOU@users.noreply.github.com>
2022-12-11 22:09:07 -0500

    Add files via upload

---
+++ README.md
@@ -0,0 +1,26 @@
+# Evaluate effects of different kind of prior knowledge in causal structure learning from data
+## The proposed project
+The goal of this project is to examine on how the prior knowledge affect the result of learning causal structure from a given dataset. In specific, the incorrect edges are added to the blacklist and white list as prior knowledge for the examination of the result.
+
+> Experiments are performed to understand the relationship between informativeness of the prior and dataset sample size. From the survey of the previous works, one consistent finding was that if prior beliefs are closer to the true model, then they improve the learning process but if priors are misleading or incorrect, they harm the learning process [1]. The thought is to further understand this phenomenon by doing various experiments which systematically vary dataset size and prior correctness. The project will also focus on understanding the relationship between how complicated the prior is (expressiveness of prior e.g. edge orientation vs path) and learning accuracy.
+
+> - Vary the correctness of the prior provided by the user of the algorithm and run algorithm for fixed dataset size. This can be done by providing arguments to the structure learning algorithm in the form of blacklists and whitelists.
+> - Vary the size of the dataset and keep the correctness of the prior fixed. 
+
+
+
+## Dataset
+The project mainly used the [*alarm*](https://www.bnlearn.com/documentation/man/alarm.html) Dataset.
+
+## The ground truth Causal Structure
+![ground_truth_dag](figures/alarm_ground_truth_dag.png)
+
+## Learn a causal structure
+F1 score is used as the scorer function to compare the learned network with ground truth
+
+## Figures
+![F1 score given 1 incorrect blacklist edge](figures/blacklist_1_f1.png)
+![F1 score given n random incorrect blacklist edge](figures/blacklist_n_f1.png)
+![F1 score given 1 correct whitelist edge](figures/whitelist_c1_f1.png)
+![F1 score given n random correct whitelist edge](figures/whitelist_cn_f1.png)
+![F1 score given 20 random incorrect blacklist edge](figures/blacklist_size_20_f1.png)
---
+++ alarm.R
@@ -0,0 +1,197 @@
+# # Install packages
+# install.packages(c("bnlearn", "bnviewer"))
+# if (!requireNamespace("BiocManager", quietly = TRUE)) 
+#   install.packages("BiocManager")
+# BiocManager::install()
+# BiocManager::install(c("graph", "Rgraphviz"))
+# install.packages("ggplot2")
+
+# Load packages
+library("bnlearn")
+library("bnviewer")
+library("Rgraphviz")
+library("ggplot2")
+
+setwd('~/Projects/cics490e_research')
+# setwd('E:/Projects/cics490e_research')
+
+# Compute f1 score given tp, fp, fn
+f1 <- function(m) {
+  tp <- m$tp
+  fp <- m$fp
+  fn <- m$fn
+
+  return(tp / (tp + (fp + fn) / 2))
+}
+
+# Load Dataset
+data('alarm')
+head(alarm)
+
+# Ground truth network
+modelstring <- paste0("[HIST|LVF][CVP|LVV][PCWP|LVV][HYP][LVV|HYP:LVF][LVF]",
+                      "[STKV|HYP:LVF][ERLO][HRBP|ERLO:HR][HREK|ERCA:HR][ERCA][HRSA|ERCA:HR][ANES]",
+                      "[APL][TPR|APL][ECO2|ACO2:VLNG][KINK][MINV|INT:VLNG][FIO2][PVS|FIO2:VALV]",
+                      "[SAO2|PVS:SHNT][PAP|PMB][PMB][SHNT|INT:PMB][INT][PRSS|INT:KINK:VTUB][DISC]",
+                      "[MVS][VMCH|MVS][VTUB|DISC:VMCH][VLNG|INT:KINK:VTUB][VALV|INT:VLNG]",
+                      "[ACO2|VALV][CCHL|ACO2:ANES:SAO2:TPR][HR|CCHL][CO|HR:STKV][BP|CO:TPR]")
+dag_true <- model2network(modelstring)
+graphviz.plot(dag_true)
+
+
+# Given 1 incorrect edge to blacklist
+n <- dim(dag_true$arcs)[1]
+arcs <- dag_true$arcs
+df_b1 <- data.frame(edge=character(), f1=numeric())
+
+for (i in 1:n) {
+  e <- arcs[i,]
+  net <- hc(alarm, blacklist = e)
+
+  df_b1[i,] <- c(paste(e, collapse = ' -> '), f1(compare(dag_true, net)))
+}
+df_b1$f1 = as.numeric(df_b1$f1)
+
+gt_f1 = f1(compare(dag_true, hc(alarm)))
+
+ggplot(df_b1, aes(x=edge, y=f1, group=1)) +
+  scale_x_discrete(limits=df_b1$edge) +
+  scale_y_continuous(breaks = sort(c(seq(min(df_b1$f1), max(df_b1$f1), length.out=5), gt_f1))) +
+  geom_point() +
+  geom_hline(aes(yintercept=gt_f1, color='red')) +
+  geom_text(aes(5,gt_f1,label = 'Without given prior knowledge', vjust = -0.5, color='red')) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(title = "F1 score given 1 incorrect blacklist edge",
+       x='Edge added to the blacklist', y='F1 Score') +
+  theme(legend.position = "none")
+ggsave(
+  'blacklist_1_f1.png',
+  device = 'png',
+  path = 'figures',
+  width = 32,
+  height = 18,
+  units = 'cm'
+)
+
+# Given n random incorrect edge to blacklist
+df_bn <- data.frame(f1=numeric())
+
+for (i in 1:n) {
+  e <- arcs[sample(1:n, i),]
+  net <- hc(alarm, blacklist = e)
+
+  df_bn[i,] <- f1(compare(dag_true, net))
+}
+
+ggplot(df_bn, aes(x=(1:n), y=f1)) +
+  scale_y_continuous(breaks = sort(c(seq(min(df_bn$f1), max(df_bn$f1), length.out=5), gt_f1))) +
+  geom_point() +
+  geom_hline(aes(yintercept=gt_f1, color='red')) +
+  geom_text(aes(5,gt_f1,label = 'Without given prior knowledge', vjust = -0.5, color='red')) +
+  labs(title = "F1 score given n random incorrect blacklist edge",
+       x='Number of edges added to the blacklist', y='F1 Score') +
+  theme(legend.position = "none")
+ggsave(
+  'blacklist_n_f1.png',
+  device = 'png',
+  path = 'figures',
+  width = 32,
+  height = 18,
+  units = 'cm'
+)
+
+
+# Given 1 correct edge to the white list
+df_cw1 <- data.frame(edge=character(), f1=numeric())
+
+for (i in 1:n) {
+  e <- arcs[i,]
+  net <- hc(alarm, whitelist = e)
+
+  df_cw1[i,] <- c(paste(e, collapse = ' -> '), f1(compare(dag_true, net)))
+}
+df_cw1$f1 = as.numeric(df_cw1$f1)
+
+ggplot(df_cw1, aes(x=edge, y=f1, group=1)) +
+  scale_x_discrete(limits=df_cw1$edge) +
+  scale_y_continuous(breaks = sort(c(seq(min(df_cw1$f1), max(df_cw1$f1), length.out=5), gt_f1))) +
+  geom_point() +
+  geom_hline(aes(yintercept=gt_f1, color='red')) +
+  geom_text(aes(5,gt_f1,label = 'Without given prior knowledge', vjust = -0.5, color='red')) +
+  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1)) +
+  labs(title = "F1 score given 1 correct whitelist edge",
+       x='Edge added to the whitelist', y='F1 Score') +
+  theme(legend.position = "none")
+ggsave(
+  'whitelist_c1_f1.png',
+  device = 'png',
+  path = 'figures',
+  width = 32,
+  height = 18,
+  units = 'cm'
+)
+
+
+# Given n random correct edge to the white list
+df_cwn <- data.frame(f1=numeric())
+
+for (i in 1:n) {
+  e <- arcs[sample(1:n, i),]
+  net <- hc(alarm, whitelist = e)
+
+  df_cwn[i,] <- f1(compare(dag_true, net))
+}
+
+ggplot(df_cwn, aes(x=(1:n), y=f1)) +
+  scale_y_continuous(breaks = sort(c(seq(min(df_cwn$f1), max(df_cwn$f1), length.out=5), gt_f1))) +
+  geom_point() +
+  geom_hline(aes(yintercept=gt_f1, color='red')) +
+  geom_text(aes(5,gt_f1,label = 'Without given prior knowledge', vjust = -0.5, color='red')) +
+  labs(title = "F1 score given n random correct whitelist edge",
+       x='Number of edges added to the whitelist', y='F1 Score') +
+  theme(legend.position = "none")
+ggsave(
+  'whitelist_cn_f1.png',
+  device = 'png',
+  path = 'figures',
+  width = 32,
+  height = 18,
+  units = 'cm'
+)
+
+
+# Given 20 random incorrect edge to the blacklist and learn the network from different size of data
+df_size_b20 <- data.frame(size=numeric(), f1=numeric())
+
+e <- arcs[sample(1:n, 20),]
+for (i in c(100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000)) {
+  sim <- rbn(dag_true, i, alarm, replace.unidentifiable = TRUE)
+
+  net <- hc(sim, blacklist = e)
+
+  df_size_b20[dim(df_size_b20)[1]+1,] <- c(i, f1(compare(dag_true, net)))
+}
+df_size_b20$size = as.numeric(df_size_b20$size)
+df_size_b20$f1 = as.numeric(df_size_b20$f1)
+
+ggplot(df_size_b20, aes(x=size, y=f1)) +
+  scale_x_continuous(trans='log10') +
+  scale_y_continuous(breaks = sort(c(seq(min(df_size_b20$f1), max(df_size_b20$f1), length.out=5), gt_f1))) +
+  geom_point() +
+  geom_hline(aes(yintercept=gt_f1, color='red')) +
+  geom_text(aes(200,gt_f1,label = 'Without given prior knowledge', vjust = -0.5, color='red')) +
+  labs(title = "F1 score given 20 random incorrect blacklist edge and learn the network from different size of data",
+       x='Size of data the network learned from (log_10)', y='F1 Score') +
+  theme(legend.position = "none")
+ggsave(
+  'blacklist_size_20_f1.png',
+  device = 'png',
+  path = 'figures',
+  width = 32,
+  height = 18,
+  units = 'cm'
+)
+
+
+
+save.image('data.RData')
